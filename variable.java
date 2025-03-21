26c2511dd80cdbe22d475a8b3df3c727ab27c11a
rakeshkumar558 <rakesh.vaddepalli@gmail.com>
2025-03-21 10:05:43 +0000

    demofiles

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class VariablesExample {
+	    public static void main(String[] args) {
+		            int age = 25;
+			            double price = 99.99;
+				            char grade = 'A';
+					            boolean isJavaFun = true;
+
+						            System.out.println("Age: " + age);
+							            System.out.println("Price: " + price);
+								            System.out.println("Grade: " + grade);
+									            System.out.println("Is Java Fun? " + isJavaFun);
+										        }
+}
+
